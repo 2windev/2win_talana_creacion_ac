79f7e8511efc23443e68b2e01474c12b0d225b6f
sebastixn <sebastian.alayon@2win.cl>
2024-01-12 16:55:30 -0500

    feat(libs): se añade libs para creacion de registros que contiene funcion crearCliente

---
+++ libs_talana_creacion_ac/DAO_2win_talana_creacion_ac_crear_registros.js
@@ -0,0 +1,95 @@
+/**
+ * @NApiVersion 2.1
+ * @module ./DAO_2win_iva_af_crear_registros.js
+ * @NModuleScope Public
+ */
+define(["N/record","N/format","N/error","./DAO_controlador_errores.js"], function(record,format,errorModule,controladorErrores){
+
+    /**.
+    * @function crearCliente - Crear un nuevo cliente en la tabla customer.
+    * @param {Object} datos - Datos para los campos del cliente a crear.
+    * @return {String} - Internalid del cliente creado.
+    */
+    function crearCliente(datos) {
+        try{
+            log.debug("crearCliente - datos", {
+                "datos": datos,
+                "tipoDato": typeof(datos)
+            })
+
+            datos.razonSocial.proceso.etapa = "crearCliente"
+            datos.acuerdoComercial.proceso.etapa = "crearCliente"
+            datos.razonSocial.proceso.externalId = datos.razonSocial.proceso.idCluster + "_" + datos.razonSocial.id + "_" + datos.acuerdoComercial.id
+            datos.razonSocial.proceso.digitoVerificador = datos.razonSocial.rut[datos.razonSocial.rut.length - 1]
+            // datos.acuerdoComercial.proceso.detalleAcuerdoComercial = "Acuerdo comercial № " + datos.acuerdoComercial.id + "\nRazón social pagadora № " + datos.razonSocial.id + " " +  datos.razonSocial.rut + " " + datos.razonSocial.razonSocial + "\n\nPlan Contratado: True \n\nBillinCycle: " + datos.acuerdoComercial.billingCycle + "\n\nNotas: " + datos.acuerdoComercial.notes + "\n\nPlan contratado: \n" + datos.acuerdoComercial.hired_plan
+
+            datos.acuerdoComercial.proceso.detalleAcuerdoComercial = `<p>Acuerdo comercial № ${datos.acuerdoComercial.id} <br>Razón social pagadora № ${datos.razonSocial.id} ${datos.razonSocial.rut} ${datos.razonSocial.razonSocial}<br></p><p> Plan Contratado: True </p><p>BillingCycle: ${datos.acuerdoComercial.billingCycle}</p><p> Notas: ${datos.acuerdoComercial.notes}</p><p>Plan Contratado: <pre>${datos.acuerdoComercial.hired_plan}</pre> </p>`
+
+            log.debug("crearCliente - datos - modificados", datos)
+            log.debug("crearCliente - datos.razonSocial", datos.razonSocial)
+            log.debug("crearCliente - datos.acuerdoComercial", datos.acuerdoComercial)
+
+            // Crear registro
+            var registro = record.create({ type : record.Type.CUSTOMER, isDynamic: true })
+
+            // Definir campos registro
+            // registro.setValue({ fieldId: "entityid", value: datos.clienteNombre });
+            // log.debug ("crearCliente - bodyFields","entityid");
+            registro.setValue({ fieldId: "externalid", value: datos.razonSocial.proceso.externalId });
+            log.debug ("crearCliente - bodyFields","externalid");
+            registro.setValue({ fieldId: "custentity_lmry_digito_verificator", value: datos.razonSocial.proceso.digitoVerificador }); 
+            log.debug ("crearCliente - bodyFields","custentity_lmry_digito_verificator");
+            registro.setValue({ fieldId: "custentity_lmry_countrycode", value: datos.razonSocial.rut });
+            log.debug ("crearCliente - bodyFields","custentity_lmry_countrycode");
+            registro.setValue({ fieldId: "custentity_lmry_nombre_empresa", value: datos.razonSocial.razonSocial });
+            log.debug ("crearCliente - bodyFields","custentity_lmry_nombre_empresa");
+            registro.setValue({ fieldId: "custentity_lmry_giro_libre", value: datos.razonSocial.giro });
+            log.debug ("crearCliente - bodyFields","custentity_lmry_giro_libre");
+            registro.setValue({ fieldId: "defaultaddress", value: datos.razonSocial.direccion });
+            log.debug ("crearCliente - bodyFields","defaultaddress");
+            registro.setValue({ fieldId: "phone", value: datos.razonSocial.telefono });
+            log.debug ("crearCliente - bodyFields","phone");
+            registro.setValue({ fieldId: "custentity_tal_ca_pk", value: "3896" }); // datos.razonSocial.rutRepresentanteLegal
+            log.debug ("crearCliente - bodyFields","custentity_tal_ca_pk");
+            registro.setValue({ fieldId: "email", value: "fpgarcia@cmvm.cl" }); // datos.razonSocial.logo
+            log.debug ("crearCliente - bodyFields","email");
+            registro.setValue({ fieldId: "comments", value: datos.razonSocial.empresa_est });
+            log.debug ("crearCliente - bodyFields","comments");
+
+            registro.setValue({ fieldId: "custentity_lmry_nomolestar", value: datos.acuerdoComercial.no_molestar });
+            log.debug ("crearCliente - bodyFields","custentity_lmry_nomolestar");
+            registro.setValue({ fieldId: "custentity_lmry_enimplementacion", value: datos.acuerdoComercial.en_implementacion });
+            log.debug ("crearCliente - bodyFields","custentity_lmry_enimplementacion");
+            registro.setValue({ fieldId: "custentity_2winestadoacuerdo", value: datos.acuerdoComercial.status });
+            log.debug ("crearCliente - bodyFields","custentity_2winestadoacuerdo");
+            registro.setValue({ fieldId: "custentity_2winonhold", value: datos.acuerdoComercial.on_hold });
+            log.debug ("crearCliente - bodyFields","custentity_2winonhold");
+            registro.setValue({ fieldId: "custentity_2winmotivoonhold", value: datos.acuerdoComercial.on_hold_reason });
+            log.debug ("crearCliente - bodyFields","custentity_2winmotivoonhold");
+            registro.setValue({ fieldId: "custentity_lmry_requiereoc", value: datos.acuerdoComercial.requires_oc });
+            log.debug ("crearCliente - bodyFields","custentity_lmry_requiereoc");
+            registro.setValue({ fieldId: "custentity_tal_commercialaggrdetails", value: datos.acuerdoComercial.proceso.detalleAcuerdoComercial });
+            log.debug ("crearCliente - bodyFields","custentity_tal_commercialaggrdetails");
+
+            // Guardar registro
+            var idCliente = registro.save({ enableSourcing: true, ignoreMandatoryFields: true });
+            log.audit("crearCliente - idCliente", idCliente)
+
+            datos.razonSocial.proceso.idCliente = idCliente
+            datos.acuerdoComercial.proceso.idCliente = idCliente
+
+            return datos
+        }catch(error){
+            log.error("crearCliente - error", error);
+            datos.razonSocial.proceso.idCliente = ""
+            datos.razonSocial.proceso.estado = "001"
+            datos.razonSocial.proceso.resultado = error.message
+
+            return datos
+        }
+    }
+    
+    return {
+        crearCliente: crearCliente
+    }
+});
